b9276f766d920b122ba2c6fc70b9b0ca59c3e490
Angel Austria <einjel.austria@gmail.com>
2023-03-31 15:30:55 +0800

    Added list of schedule based on following users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,12 @@
 class Api::V1::UsersController < ApplicationController
+  def index
+    user = User.find(params[:user_id])
+    records = SleepSchedule.by_user(user.following_users.pluck(:follower_id)).by_past_week
+                  &.as_json(only: %i[id], methods: %i[owner alarm_date sleep_time_hour wake_time_hour total_sleep_hour])
+                  .sort_by { |record| record['total_sleep_hour'] }.reverse
+
+    render json: { user_id: user.id, records: }, status: :ok
+  end
 
   def create
     user = User.create(user_params)
